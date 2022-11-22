5192e6c82edc97c4bdeb565a8c32580bf10f89da
Pablo <nunezp20@gmail.com>
2022-11-22 00:22:03 -0600

    private page edited

--- src/app/pages/generator-private/generator-private.component.ts
+++ src/app/pages/generator-private/generator-private.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
 @Component({
   selector: 'app-generator-private',
   templateUrl: './generator-private.component.html',
   styleUrls: ['./generator-private.component.scss']
 })
-export class GeneratorPrivateComponent implements OnInit {
-
-  constructor() { }
+export class GeneratorPrivateComponent implements OnInit, OnChanges{
+  user: any;
+  constructor(router: Router, private authService: AuthService) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
+    this.user = this.authService.getUser()
+    this.authService.onUser.subscribe(
+      (lang) => {
+        this.user = this.authService.getUser();
+      }
+    );
   }
 
 }
