1205c200dc94f3231bbaa5c9204ac59097ed424f
jpdealba <jpdeaty1207@gmail.com>
2022-11-29 22:28:25 -0600

    sockets working

--- src/app/shared/services/socket.service.ts
+++ src/app/shared/services/socket.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable, Output } from '@angular/core';
+import { Subject } from 'rxjs';
 import * as socketIo from 'socket.io-client';
+import { CoinComponent } from 'src/app/pages/coin/coin.component';
 import { environment } from 'src/environments/environment';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -9,10 +10,13 @@ export class SocketService {
   url = environment.apiUrl;
   socket: any = null;
   room: string = '';
-
+  addTransaction: Subject<any> = new Subject<any>();
   constructor() { }
 
   setRoom(room: string) {
+    this.socket.disconnect()
+    this.socket = null
+    this.connect()
     this.room = room;
     this.socket.emit('join-room', room);
   }
@@ -21,18 +25,17 @@ export class SocketService {
     this.socket = socketIo.io(this.url);
     this.socket.on('connect', () => {
       console.log(`Connected to ${this.url}`);
+      this.subscribeToChanges()
     });
   }
 
   subscribeToChanges(): void {
-    // this.socket.on("transaction-case", (data: any) => {
-    //   console.log("transaction-case")
-    //   console.log(data)
-    // });
-    this.socket.fromEvent("transaction-case")
-      .subscribe((e: any) => {
-        console.log(e);
-      });
+    this.socket.on("transaction-case", (data: any) => {
+      if (data.symbol_from == this.room || data.symbol_to == this.room) {
+        this.addTransaction.next(data);
+      }
+
+    });
   }
 
   sendTransaction(data: any) {
